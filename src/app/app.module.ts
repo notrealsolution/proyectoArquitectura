aced0c16e0a562d9077f379694d3083b87ad0292
Daniel Antonio Sánchez Jiménez <notrealsolution@gmail.com>
2024-02-17 21:37:25 -0600

    Agregando componente nosotros, primeras funciones

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,13 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
+import { SobreNosotrosComponent } from './sobre-nosotros/sobre-nosotros.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent
+    HeaderComponent,
+    SobreNosotrosComponent
   ],
   imports: [
     BrowserModule
