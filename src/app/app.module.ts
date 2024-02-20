3aa3b06f51a21c9cd11ee32a8553f7d02c0f4dac
Daniel Antonio Sánchez Jiménez <notrealsolution@gmail.com>
2024-02-19 20:10:56 -0600

    Iniciado contenido main de la página principal

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { SobreNosotrosComponent } from './sobre-nosotros/sobre-nosotros.component';
+import { ModeloComponent } from './modelo/modelo.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    SobreNosotrosComponent
+    SobreNosotrosComponent,
+    ModeloComponent
   ],
   imports: [
     BrowserModule
