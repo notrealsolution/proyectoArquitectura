ab61a50df731817159cfa3432c8a5e8a1e767114
Daniel Antonio Sánchez Jiménez <notrealsolution@gmail.com>
2024-02-22 22:04:15 -0600

    Creacion de componente galería

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,13 +5,15 @@ import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { SobreNosotrosComponent } from './sobre-nosotros/sobre-nosotros.component';
 import { ModeloComponent } from './modelo/modelo.component';
+import { GaleriaComponent } from './galeria/galeria.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
     SobreNosotrosComponent,
-    ModeloComponent
+    ModeloComponent,
+    GaleriaComponent
   ],
   imports: [
     BrowserModule
